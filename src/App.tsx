a4b771acf4aa29691fb979cc6377c58a8d5a1300
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-06-16 13:41:38 -0300

    aulão de testes com react-testing-library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
 function App() {
+  const [message, setMessage] = useState(
+    "Let's learn more about testing in React"
+  );
   return (
     <div>
       <h1>Hello world!</h1>
-      <p>Let's learn more about testing in React</p>
+      <p>{message}</p>
+      <button onClick={() => setMessage("New Message")}>Change message</button>
+      <Button
+        disabled={false}
+        onClick={() => {
+          setMessage("New Message 2.0");
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
 }
